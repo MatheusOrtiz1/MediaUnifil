1cee30e45beead014a2f1d041ae8dbfdfe5b2160
matheus.vieira <matheusortiz43ful@hotmail.com>
2023-07-10 18:20:21 -0300

    Mensagem adicionada

--- unifil.py
+++ unifil.py
@@ -2,10 +2,10 @@ import PySimpleGUI as sg
 
 sg.theme('DarkAmber')   # Adiciona um toque de cor
 # Todos os elementos dentro da janela.
-layout = [  [sg.Text('AVA1'), sg.InputText()],
-            [sg.Text('AVA2'), sg.InputText()],
-            [sg.Text('AVA3'), sg.InputText()],
-            [sg.Text('AVA4'), sg.InputText()],
+layout = [  [sg.Text('AVA1'), sg.InputText(key='ava1')],
+            [sg.Text('AVA2'), sg.InputText(key='ava2')],
+            [sg.Text('AVA3'), sg.InputText(key='ava3')],
+            [sg.Text('AVA4'), sg.InputText(key='ava4')],
             [sg.Button('Calcular'), sg.Button('Sair')]]
 
 # Cria a janela
@@ -16,8 +16,12 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Sair': # Se o usuário fechar a janela ou clicar em cancelar
         break
-    avas = [float(values[0]), float(values[1]), float(values[2]), float(values[3])]
+    avas = [float(values['ava1']), float(values['ava2']), float(values['ava3']), float(values['ava4'])]
     media = avas[0]*0.1 + avas[1]*0.3 + avas[2]*0.2 + avas[3]*0.4
-    sg.popup(f'Sua média é {media}')
+
+    if media >= 70:
+        sg.popup(f'Parabéns! Você foi aprovado com média {media:.2f}!')
+    else:
+        sg.popup(f'Infelizmente você foi reprovado com média {media:.2f}.')
 
 window.close()
